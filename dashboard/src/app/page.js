5cdce4153c0d489a66525c82038c542fb36c5158
Rao <103417697+Aby-ss@users.noreply.github.com>
2025-01-21 19:34:22 +0400

    Updated file drag and drop system ✅

--- dashboard/src/app/page.js
+++ dashboard/src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 
-const fileTypes = ["JPG", "PNG", "GIF"];
+const fileTypes = ["JPG", "PNG", "GIF", "CSV"];
 
 export default function Home() {
   const [file, setFile] = useState(null);
@@ -11,16 +11,16 @@ export default function Home() {
         setFile(URL.createObjectURL(uploadedFile));
     };
   return (
-    <div className="flex flex-col gap-10 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col gap-6 relative top-20 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="font-bold text-3xl tracking-[-.05em]">
-        Insert your file below
+        Reachify : Automated Client Outreach [Beta testing]
       </h1>
       <p className="font-semibold text-xl tracking-[-.05em] w-[700px] text-center">
         Drag and drop your file containing the prospect detials in the area below. File type supported : csv excel files
       </p>
 
-      <div className="flex flex-col items-center gap-5 border-dashed border-2 border-sky-500 w-[500px] h-[250px] p-[16px] rounded-md flex justify-center">
-        <p className="font-bold text-lg tracking-[-.05em]">Drag n drop the files</p>
+      <div className="flex flex-col items-center gap-5 border-dashed border-8 border-sky-500 w-[500px] h-[250px] p-[16px] rounded-md flex justify-center">
+        <p className="font-bold text-2xl tracking-[-.05em]">Drag n drop the files</p>
 
         <div>
             <FileUploader 
